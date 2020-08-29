be4c67299d992010389f6ea71118d984a4a44025
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2020-08-28 17:46:58 -0700

    add a first draft of the back end logic.

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+function getBaseAbilityScore(){
+  return parseInt(Math.random() * 4 + 4)
+}
+
+let str = getBaseAbilityScore();
+let con = getBaseAbilityScore();
+let dex = getBaseAbilityScore();
+let int = getBaseAbilityScore();
+let wis = getBaseAbilityScore();
+let cha = getBaseAbilityScore();
+
+$(document).ready(function() {
+  $("form#CreateCharacter").submit(function(event) {
+    event.preventDefault();
+    const creature = $("input:radio[name=creature]:checked").val();
+    const upbringing = $("input:radio[name=upbringing]:checked").val();
+    const job = $("input:radio[name=job]:checked").val();
+    const habit = $("input:radio[name=habit]:checked").val();
+    
+    if (creature === 'human'){
+      str += 100;
+    } else if (creature === 'elf'){
+      con += 100;
+    } else if (creature === 'dwarf'){
+      dex += 100;
+    } else if (creature === 'orc'){
+      int += 100;
+    } else if (creature === 'goblin'){
+      wis += 100;
+    } else if (creature === 'kobold'){
+      cha += 100;
+    }
+
+    if (upbringing === 'cave'){
+      str += 100;
+    } else if (upbringing === 'city'){
+      con += 100;
+    } else if (upbringing === 'hut'){
+      dex += 100;
+    } else if (upbringing === 'valley'){
+      int += 100;
+    } else if (upbringing === 'wizard'){
+      wis += 100;
+    } else if (upbringing === 'slave'){
+      cha += 100;
+    }
+
+    if (job === 'soldier'){
+      str += 100;
+    } else if (job === 'bandit'){
+      con += 100;
+    } else if (job === 'deacon'){
+      dex += 100;
+    } else if (job === 'engineer'){
+      int += 100;
+    } else if (job === 'farmer'){
+      wis += 100;
+    } else if (job === 'fur'){
+      cha += 100;
+    }
+
+    if (habit === 'fights'){
+      str += 100;
+    } else if (habit === 'steals'){
+      con += 100;
+    } else if (habit === 'drugs'){
+      dex += 100;
+    } else if (habit === 'nudity'){
+      int += 100;
+    } else if (habit === 'dirty'){
+      wis += 100;
+    } else if (habit === 'debate'){
+      cha += 100;
+    }
+    
+    
+    
+    
+    
+    $("#str").text(str);
+    $("#con").text(con);
+    $("#dex").text(dex);
+    $("#int").text(int);
+    $("#wis").text(wis);
+    $("#cha").text(cha);
+    $("#result").show();
+
+  });
+});
